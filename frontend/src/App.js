1d61704399200dfef454dbe7dee0956844cfe08e
zzzkorn <pavelevseev92@yandex.ru>
2022-12-14 17:31:37 +0300

    feat: some feat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+
+import PonyNote from "./components/PonyNote";
+import NotFound from "./components/NotFound";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<PonyNote/>} > </Route>
+          <Route exact path="*" element={<NotFound/>} > </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
