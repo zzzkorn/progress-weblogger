45da4e89d53c69143190bbada84592a44ecc4d33
zzzkorn <pavelevseev92@yandex.ru>
2022-12-23 16:26:25 +0300

    feat: main page

--- frontend/ng-prj/src/app/app.module.ts
+++ frontend/ng-prj/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -13,6 +16,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
