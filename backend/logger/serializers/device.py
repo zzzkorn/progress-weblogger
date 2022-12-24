cdd1f89de7f56921227653ac204735a72735ba1b
zzzkorn <pavelevseev92@yandex.ru>
2022-12-24 11:03:05 +0300

    refactor: update message generator, add device information into message

--- backend/logger/serializers/device.py
+++ backend/logger/serializers/device.py
@@ -2,6 +2,12 @@ from logger.models import Device
 from rest_framework import serializers
 
 
+class DeviceShortSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Device
+        fields = ["id", "ip_address", "description"]
+
+
 class DeviceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Device
